35a1e2b1d72eb8c7a51731c6baf09e44c18a267c
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-20 20:52:09 -0500

    💄 Add styles to Grid, make Image a StyledComponent so it'll work

--- src/components/presentational/Grid.js
+++ src/components/presentational/Grid.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Image from './Image';
+import styled from 'styled-components';
 
-const Grid = (props) => (
-  <div className="grid">
+
+const UnstyledGrid = (props) => (
+  <div className={props.className}>
     {props.images.map((image, index) => {
       return <Image key={`grid-image-${index}`} src={image.src} alt={image.alt}/>
     })}
   </div>
 )
+
+const Grid = styled(UnstyledGrid)`
+  width: 100%;
+  display: grid;
+  grid: repeat(3, 1fr)/ repeat(5, 1fr);
+  gap: 10px;
+
+  ${Image} {
+    width: 100%;
+  }
+`
 
 export default Grid
