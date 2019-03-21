28b348eb06cd71c2c41435f957a8300917fe04f4
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-21 14:55:57 -0500

    🎨 Add some pretty padding around the Grid

--- src/components/presentational/Grid.js
+++ src/components/presentational/Grid.js
@@ -16,6 +16,7 @@ const Grid = styled(UnstyledGrid)`
   display: grid;
   grid: repeat(auto-fit,  minmax(200px, 1fr))/ repeat(auto-fit,  minmax(200px, 1fr));
   gap: 10px;
+  padding: 10px;
 
   ${Image} {
     width: 100%;
