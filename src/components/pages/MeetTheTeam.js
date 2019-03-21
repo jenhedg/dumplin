f833ec924119fc44017dd8dcda217ebdd05e0e39
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-21 18:19:41 -0500

    Wrap text at a resonable length

--- src/components/pages/MeetTheTeam.js
+++ src/components/pages/MeetTheTeam.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const MeetTheTeam = () => (
   <p>
-    The team at Crescendo Collective is designed to outpace and outperform larger agencies; we combine industry-leading
-    technology with a work ethic rooted in trust and service to clients. Let us partner with you and prove why we’re the
-    most advanced digital agency in the Midwest.
+    The team at Crescendo Collective is designed to outpace and outperform
+    larger agencies; we combine industry-leading technology with a work ethic
+    rooted in trust and service to clients. Let us partner with you and prove
+    why we’re the most advanced digital agency in the Midwest.
   </p>
 );
 
