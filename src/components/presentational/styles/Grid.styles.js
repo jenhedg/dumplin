74d0ca2e148c18beace2736c68b01055cc97ac12
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-21 16:38:24 -0500

    🎨  Add some pretty animations to the Grid

--- src/components/presentational/styles/Grid.styles.js
+++ src/components/presentational/styles/Grid.styles.js
@@ -8,10 +8,20 @@ export default Grid => styled(Grid)`
       auto-fit,
       minmax(200px, 1fr)
     );
-  gap: 10px;
-  padding: 10px;
+  /* gap: 10px; */
+  /* padding: 10px; */
 
   ${Image} {
-    width: 100%;
+    width: 100%
+    position: relative;
+    transform: scale(.95);
+    box-shadow: 2px 2px 5px 0px rgba(0,0,0,.3);
+    transition: transform 150ms cubic-bezier(.28,0,.39,1), box-shadow 150ms cubic-bezier(.28,0,.39,1);
+  }
+
+  ${Image}:hover {
+    transform: scale(1);
+    z-index: 1;
+    box-shadow: 2px 2px 10px 0px rgba(0,0,0,.5);
   }
 `;
