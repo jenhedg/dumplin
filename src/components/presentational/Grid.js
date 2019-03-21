8f60146ee6972588c18858fa2a5bee96270d5ba2
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-20 20:17:42 -0500

    Get rid of desctructuring for clarity

--- src/components/presentational/Grid.js
+++ src/components/presentational/Grid.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Image from './Image';
 
-const Grid = (props) => {
-  const { images } = props;
-
-  return (
-    <div className="grid">
-      {images.map((image, index) => {
-        return <Image key={`grid-image-${index}`} src={image.src} alt={image.alt}/>
-      })}
-    </div>
-  )
-}
+const Grid = (props) => (
+  <div className="grid">
+    {props.images.map((image, index) => {
+      return <Image key={`grid-image-${index}`} src={image.src} alt={image.alt}/>
+    })}
+  </div>
+)
 
 export default Grid
