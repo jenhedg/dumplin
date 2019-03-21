7df56a9efa0f275a79e846d6af7006284bb32868
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-21 14:35:45 -0500

    add some global styles to get rid of the margin and padding of html

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import MainNav from './container/MainNav';
 import MainHero from './container/MainHero';
+import { createGlobalStyle } from 'styled-components'
 import RandomImagesGrid from './container/RandomImagesGrid';
+
+const AppStyle = createGlobalStyle`
+  @import('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans');
+
+  html, 
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: "Roboto", Arial, Helvetica, sans-serif;
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppStyle/>
         <MainNav/>
         <MainHero/>
         <RandomImagesGrid/>
