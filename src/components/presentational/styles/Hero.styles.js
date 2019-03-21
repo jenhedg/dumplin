625bdb0a557f8222dbae924dff370edae0951006
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-21 17:22:40 -0500

    🎨 Add a background screen to Hero

--- src/components/presentational/styles/Hero.styles.js
+++ src/components/presentational/styles/Hero.styles.js
@@ -4,6 +4,7 @@ export default Hero => styled(Hero)`
   width: auto;
   height: calc(100vh - 50px);
   overflow: hidden;
+  position: relative;
 
   .hero__title {
     position: absolute;
@@ -12,5 +13,16 @@ export default Hero => styled(Hero)`
     transform: translate(-50%, -50%);
     color: white;
     font-size: 32px;
+    z-index: 1;
+  }
+
+  &:after {
+    content: ' ';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.8);
   }
 `;
