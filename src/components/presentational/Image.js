bacef1118942796b530aab2805cc6f41694d156b
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-20 20:52:09 -0500

    💄 Add styles to Grid, make Image a StyledComponent so it'll work

--- src/components/presentational/Image.js
+++ src/components/presentational/Image.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Image = (props) => <img src={props.src} alt={props.alt}/>
+
+const UnstyledImage = (props) => <img className={props.className} src={props.src} alt={props.alt}/>
+
+const Image = styled(UnstyledImage)``;
 
 export default Image
