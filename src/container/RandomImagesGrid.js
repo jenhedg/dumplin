0a79527c2b6a0dea6cf60154f1255d6ba1967498
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-20 20:09:53 -0500

    Add a grid of random images

---
+++ src/container/RandomImagesGrid.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '../presentational/Grid';
+import {images} from '../consts';
+class RandomImagesGrid extends React.Component {
+
+  state = {
+    images: [] // Start with an empty array
+  }
+
+  componentDidMount(){
+    this.setState(state => {
+      return { images: images }
+    })
+  }
+  
+  render(){
+    return (
+      <Grid images={this.state.images}/>
+    )
+  }
+}
+
+
+export default RandomImagesGrid
