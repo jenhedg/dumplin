6457461e7c1f421c73bc2602e4a6e8eb9bac317e
Trent Rogers <trentkrogers@gmail.com>
2019-03-20 20:55:16 -0500

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Link, Route, Switch,
+} from 'react-router-dom';
 import MainNav from './container/MainNav';
 import MainHero from './container/MainHero';
 import RandomImagesGrid from './container/RandomImagesGrid';
 import GlobalStyles from './presentational/styles/_Global.styles';
 
 const App = () => (
-  <div className="App">
-    <GlobalStyles />
-    <MainNav />
-    <MainHero />
-    <RandomImagesGrid />
-  </div>
+  <Router>
+    <div className="App">
+      <GlobalStyles />
+      <MainNav />
+      <MainHero />
+      <RandomImagesGrid />
+    </div>
+  </Router>
 );
 
 export default App;
