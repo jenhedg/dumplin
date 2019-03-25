22209d36fc727f632bd2e0a248e3a77515236091
Jennifer <jhedgcock@crescendocollective.com>
2019-03-25 15:23:24 -0500

    destructure nav to pull state items

--- src/components/presentational/Nav.js
+++ src/components/presentational/Nav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import NavLink from './NavLink';
 
-
 // / TODO: Add Styles
 
-const Nav = items => (
+const Nav = ({items}) => (
   <nav className="nav">
     <ul>
-      {items.map(({ href, title }) => {
-        return <NavLink href={href} title={title} key={`nav-link--${href}`} />;
-      })}
+      {Array.isArray(items) ? (
+        items.map(({ href, title }) => {
+          return <NavLink href={href} title={title} key={`nav-link--${href}`} />;
+        })
+      ) : (
+        <div />
+      )}
     </ul>
   </nav>
 );
