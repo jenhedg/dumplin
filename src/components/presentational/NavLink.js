0ffb7ca152346447e741c0bcf98c1befced3145e
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-25 19:30:15 -0500

    Links should be NavLinks from React Router

--- src/components/presentational/NavLink.js
+++ src/components/presentational/NavLink.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './styles/NavLink.styles';
 
 const NavLink = ({ className, href, title }) => (
   <li className={className}>
-    <a href={href}>{title}</a>
+    <Link to={href}>{title}</Link>
   </li>
 );
 
