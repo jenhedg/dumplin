93b9d72a73d31e67c1087edbbfd14e5e25b9c85b
Trent Rogers <trentkrogers@gmail.com>
2019-03-27 21:00:54 -0500

    Add redux dev tools

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,7 +2,11 @@ import { createStore } from 'redux';
 import { reducers } from '../reducers/CombineReducers';
 
 function configureStore(initialState = {}) {
-  return createStore(reducers, initialState);
+  return createStore(
+    reducers,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
 }
 
 export const store = configureStore();
