283463045633cf04146b5e1611289d177d3f1b8e
Jennifer <jhedgcock@crescendocollective.com>
2019-03-27 20:26:34 -0500

    rm weird character

--- src/components/container/MainLogo.js
+++ src/components/container/MainLogo.js
@@ -3,7 +3,7 @@ import Image from '../presentational/Image';
 import { LOGOIMAGE } from '../../consts';
 
 /// TODO: Add Styles
-ç
+
 const MainLogo = () => {
   return (
     <Image src={LOGOIMAGE.src} alt={LOGOIMAGE.alt} />
