614a93f40c5b70373b789108b9f11626a7f9582f
Theo Syslack <tsyslack@crescendocollective.com>
2019-03-27 20:13:01 -0500

    App should be a pure function

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MainNav from './container/MainNav';
 import MainHero from './container/MainHero';
 import MainLogo from './container/MainLogo';
 import EmployeeImagesGrid from './container/EmployeeImagesGrid';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MainLogo />
-        <MainNav />
-        <MainHero />
-        <EmployeeImagesGrid />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <MainLogo />
+    <MainNav />
+    <MainHero />
+    <EmployeeImagesGrid />
+  </div>
+);
 
 export default App;
